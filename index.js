60a88a6c7fbb89f767b56524ba8abdb7ad6f4eb2
bernadette <aguirre.bernadette@outlook.com>
2022-03-25 21:43:05 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(){
+    return 2000 / 100
+}
+    
+const square = (parameter) => parameter * parameter
+
+
+
+const add = (parameter1, parameter2) => {
+    console.log(`Adding ${parameter1}`);
+    console.log(`Adding ${parameter2}`);
+    return parameter1 + parameter2;
+}
